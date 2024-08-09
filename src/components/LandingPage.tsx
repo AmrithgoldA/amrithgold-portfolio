8052d4ccd684961f76f0540230afaa038ea1f0cc
amrithgold <goldandgold1002@gmail.com>
2024-08-09 11:40:36 +0530

    removed redundant file and added tailwid config plus others

--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -1,13 +1,44 @@
+import MagicButton from "../assets/Animations/magic-button";
+import { MotionUp } from "../assets/Animations/motion-up"
+import { FaLocationArrow, FaDownload } from "react-icons/fa6";
+
 export default function LandingPage() {
     return (
-        <>
-            <h1>Hi, I'm Amrith Gold, a Self Taught Software Developer</h1>
-            <p>Based in Tamil Nadu, India, Specialized in React and Node</p>
-            <button>Say Hi</button>
-            <button>Resume</button>
-            <h2>MY JOURNEY</h2>
-            <p>I embarked on my journey to become a software developer alone, without any teacher or mentor. At that time, I had some prior knowledge of programming. I seized the opportunity to learn something new during the lockdown, and it turned out to be an incredible journey. I have learned extensively from online resources, without taking any formal courses. Now, I specialize in React and Node and am capable of creating software independently.</p>
-            <h2>Strongly believe in Karma</h2>
-        </>
+        <div className="Landing-page-main-c">
+            <MotionUp delay={0}>
+                <h1 className="intro">
+                    Hi, I'm Amrith Gold, a Self Taught
+                    <br/>
+                    Software Developer
+                </h1>
+            </MotionUp>
+
+            <MotionUp delay={0.3}>
+                <p>Based in Tamil Nadu, India, Specialized in React and Node</p>
+            </MotionUp>
+
+            <MotionUp delay={0.6}>
+                <MagicButton
+                    title="Say Hi"
+                    icon={<FaLocationArrow />}
+                    position="right"
+                    scroll="#contact"
+                />
+
+                <MagicButton
+                    title="Resume"
+                    icon={<FaDownload />}
+                    position="right"
+                    externalLink={"#"}
+                />
+
+            </MotionUp>
+
+            <MotionUp delay={0.9}>
+                <h2>MY JOURNEY</h2>
+                <p>I embarked on my journey to become a software developer alone, without any teacher or mentor. At that time, I had some prior knowledge of programming. I seized the opportunity to learn something new during the lockdown, and it turned out to be an incredible journey. I have learned extensively from online resources, without taking any formal courses. Now, I specialize in React and Node and am capable of creating software independently.</p>
+                <h2>Strongly believe in Karma</h2>
+            </MotionUp>
+        </div>
     )
 };
