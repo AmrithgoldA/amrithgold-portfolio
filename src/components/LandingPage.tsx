268ad07d3c24a0ea8c449a7d2b1c3806df682b7f
amrithgold <goldandgold1002@gmail.com>
2024-08-12 22:16:51 +0530

    pushing for re deployement

--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -2,10 +2,10 @@
 
 import { MotionUp } from "../assets/Animations/Motionup"
 import { TextAppearAnimation } from "../assets/Animations/TextAppearAnimation";
+import { Highlight } from "../assets/Animations/Highlight";
 import setupImage from '../assets/images/SetupImage.png';
 import profileImage from '../assets/images/profileImage.png'
 import { FaLocationArrow, FaDownload } from "react-icons/fa6";
-import { Highlight } from "../assets/Animations/Highlight";
 
 export default function LandingPage() {
 
