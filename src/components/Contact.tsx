190a6fb3157e69f9f3760fe0971bce08b64f2a08
amrithgold <goldandgold1002@gmail.com>
2024-08-23 11:17:14 +0530

    added toast message

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -8,6 +8,8 @@ import { getSocialLinks, sendEmail } from "../api/route/ContactRoute";
 import { IoSendSharp } from "react-icons/io5";
 import { contactInfoType } from "../types/contactType";
 import { cn } from "../lib/utils";
+import { Bounce, ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 export default function Contact() {
 
@@ -21,6 +23,7 @@ export default function Contact() {
     const [socialLinks, setsocialLinks] = useState([])
     const [contactDetails, setContactDetails] = useState<contactInfoType>(contactObject)
     const [enableSubmit, setEnableSubmit] = useState<boolean>(true)
+    const [enableLoader, setEnableLoader] = useState<boolean>(false)
 
     useEffect(() => {
 
@@ -57,9 +60,27 @@ export default function Contact() {
         
     }, [contactDetails]);
 
-    const handleSubmit = () => {
-        sendEmail(contactDetails)
-        setContactDetails({...contactObject});
+    const handleSubmit = async() => {
+        setEnableLoader(true)
+
+        const promise = sendEmail(contactDetails);
+
+        toast.promise(
+            promise,
+            {
+                pending: 'Sending email...',
+                success: 'Email sent successfully!',
+                error: 'Failed to send email. Please try again. 🤯',
+            }
+        );
+    
+        const response = await promise
+
+        if(response && response.status === 200) {
+            setEnableLoader(false)
+            setContactDetails({...contactObject});
+        }
+        
     }
 
     const getSocialIcons = (title: string) => {
@@ -97,27 +118,27 @@ export default function Contact() {
         );
     };
 
-    // const LoadingIcon = () => {
-    //     return (
-    //         <svg
-    //             aria-hidden="true"
-    //             role="status"
-    //             className="inline mr-3 w-5 h-5 text-blue-100 animate-spin"
-    //             viewBox="0 0 100 101"
-    //             fill="none"
-    //             xmlns="http://www.w3.org/2000/svg"
-    //         >
-    //             <path
-    //                 d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
-    //                 fill="#E5E7EB"
-    //             ></path>
-    //             <path
-    //                 d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
-    //                 fill="currentColor"
-    //             ></path>
-    //         </svg>
-    //     );
-    // };
+    const LoadingIcon = () => {
+        return (
+            <svg
+                aria-hidden="true"
+                role="status"
+                className="inline mr-3 w-5 h-5 text-blue-100 animate-spin"
+                viewBox="0 0 100 101"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+            >
+                <path
+                    d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                    fill="#E5E7EB"
+                ></path>
+                <path
+                    d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                    fill="currentColor"
+                ></path>
+            </svg>
+        );
+    };
 
     return (
         <div className="py-10" id="contact">
@@ -228,7 +249,7 @@ export default function Contact() {
                                     <input
                                         id="email"
                                         type="email"
-                                        placeholder="amrithgold1002@gmail.com"
+                                        placeholder="goldandgold1002@gmail.com"
                                         className="flex w-full border-none bg-zinc-900 text-white shadow-input rounded-md px-3 py-2 text-sm 
                                         placeholder:text-neutral-500 
                                         focus-visible:outline-none focus-visible:ring-0
@@ -273,7 +294,7 @@ export default function Contact() {
                                     <textarea
                                         rows={3}
                                         id="message"
-                                        placeholder="Looking for a proficient software developer skilled in React and Next.js for a specific project"
+                                        placeholder="Looking for a proficient software developer skilled in React and Node.js for a specific project"
                                         className="flex w-full border-none bg-zinc-900 text-white shadow-input rounded-md px-3 py-2 text-sm 
                                         placeholder:text-neutral-500 
                                         focus-visible:outline-none focus-visible:ring-0
@@ -295,7 +316,12 @@ export default function Contact() {
                                     }}
                                 >
                                     <span className="flex w-full justify-center items-center gap-2">
-                                        Submit <IoSendSharp />
+                                        Submit 
+                                        {!enableLoader ?
+                                            <IoSendSharp />
+                                            :
+                                            <LoadingIcon />
+                                        }
                                     </span>
                                     {!enableSubmit &&
                                         <BottomGradient />
@@ -307,6 +333,19 @@ export default function Contact() {
                     </div>
                 </MotionUp>
             </div>
+            <ToastContainer
+                position="bottom-right"
+                autoClose={5000}
+                hideProgressBar={false}
+                newestOnTop={false}
+                closeOnClick
+                rtl={false}
+                pauseOnFocusLoss={false}
+                draggable
+                pauseOnHover={false}
+                theme="dark"
+                transition= {Bounce}
+            />
         </div>
     )
 };
