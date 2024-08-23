f10ae253a5b905bfcc90abc3a6667c0795d47ee5
A AMRITH GOLD <goldandgold1002@gmail.com>
2024-08-23 11:26:32 +0530

    Update client.ts

--- src/api/client.ts
+++ src/api/client.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 const client = async(url: string, method: string, body: any ) => {
 
     let config: any = {
-        url: `http://localhost:3001${url}`,
+        url: `https://amrith-portfolio-backend.onrender.com${url}`,
         method: method,
         header:{
             "content-type": 'application/json'
