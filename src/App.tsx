98ec399f86c6448b38707bf63c5e91cf669a9acd
amrithgold <goldandgold1002@gmail.com>
2024-08-26 12:08:28 +0530

    Revert "added google analytics"
    This reverts commit af111b39a586eada0752590acfea5cb0b39e87ee.

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,8 @@
 import './index.css'
 import { Route, Routes, BrowserRouter } from 'react-router-dom'
 import Portfolio from './components/Portfolio'
-import ReactGA from "react-ga4";
-
-ReactGA.initialize(import.meta.env.VITE_GOOGLE_MEASUREMENT_ID);
 
 function App() {
-
-	ReactGA.send({ hitType: "pageview", page: window.location.pathname});
 
 	return (
 		<>
