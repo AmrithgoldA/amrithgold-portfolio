b43a178918d593d0b6d405836f1a204c56afe927
amrithgold <goldandgold1002@gmail.com>
2024-09-10 20:00:13 +0530

    added updats for projects

--- src/api/client.ts
+++ src/api/client.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 const client = async(url: string, method: string, body: any ) => {
 
     let config: any = {
-        url: `http://localhost:3001${url}`,
+        url: `https://amrith-portfolio-backend.vercel.app${url}`,
         method: method,
         headers:{
             "content-type": 'application/json'
